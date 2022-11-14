71cc608678066760aa4d16c3f71c0e434d77499f
zivhousler <zivhousler@gmail.com>
2022-11-14 12:00:24 +0200

    added builder functionalities + connection to db with delete function

--- ormsystem/src/main/java/Utils/QueryBuilder.java
+++ ormsystem/src/main/java/Utils/QueryBuilder.java
@@ -1,9 +1,11 @@
 package Utils;
 
+import java.util.Map;
+
 public class QueryBuilder {
     private String query;
 
-    public String getQuery() {
+    public String toString() {
         return query;
     }
 
@@ -32,7 +34,7 @@ public class QueryBuilder {
             } else if(value instanceof String) {
                 this.query += "'" + value + "' ";
             } else {
-                throw new IllegalArgumentException("Type of element must be an integer or a string");
+                throw new IllegalArgumentException("Type of object must be an integer or a string");
             }
             return this;
         }
@@ -49,6 +51,64 @@ public class QueryBuilder {
 
         public Builder limit(int amount){
             this.query += "LIMIT " + amount;
+            return this;
+        }
+
+        public Builder delete(String tableName){
+            this.query += "DELETE FROM " + tableName + " ";
+            return this;
+        }
+
+        public Builder truncate(String tableName){
+            this.query += "TRUNCATE TABLE " + tableName + " ";
+            return this;
+        }
+
+        public <T> Builder insert(Class<T> clz){
+            this.query += "INSERT INTO " + clz.getSimpleName().toLowerCase() + " ";
+            return this;
+        }
+
+        public <V> Builder values(Map<String,V> map){
+            this.query += "(";
+
+            for (String key : map.keySet())
+                this.query += key + ", ";
+
+            this.query = this.query.substring(0, this.query.length() - 2);
+            this.query += ") VALUES (";
+
+            for (V value : map.values()){
+                if (value instanceof Integer) {
+                    this.query += value + ", ";
+                } else if(value instanceof String) {
+                    this.query += "'" + value + "', ";
+                } else {
+                    throw new IllegalArgumentException("Type of value object must be an integer or a string");
+                }
+            }
+
+            this.query = this.query.substring(0, this.query.length() - 2);
+            this.query += ") ";
+
+            return this;
+        }
+
+        public <T> Builder update(Class<T> clz){
+            this.query += "UPDATE " + clz.getSimpleName().toLowerCase() + " ";
+            return this;
+        }
+
+        public <T> Builder set(String key, T value) {
+            if (!this.query.contains("SET")) this.query += "SET ";
+            this.query += key + "=";
+            if (value instanceof Integer) {
+                this.query += value + " ";
+            } else if(value instanceof String) {
+                this.query += "'" + value + "' ";
+            } else {
+                throw new IllegalArgumentException("Type of value object must be an integer or a string");
+            }
             return this;
         }
 
