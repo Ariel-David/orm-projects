2d5d9c08812f549fcb679ce3bb7120eabab986df
zivhousler <zivhousler@gmail.com>
2022-11-15 09:05:28 +0200

    Added functionality to create an entity to the db

--- ormsystem/src/main/java/Client/Client.java
+++ ormsystem/src/main/java/Client/Client.java
@@ -16,14 +16,13 @@ public class Client {
 //        System.out.println(table.findOne(Animal.class, "numOfLegs", 4));
 //        System.out.println(table.findAny(Animal.class, "id", 2));
 
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("numOfLegs", 1);
-//        map.put("name", "ramini");
-//        map.put("sound", "riri!");
-//
-//        String query = new QueryBuilder.Builder().insert(Animal.class).values(map).build().toString();
-//        System.out.println(query);
+        Animal gato = new Animal();
+        gato.setId(1);
+        gato.setName("kitty");
+        table.createOne(gato);
 
+        String query = new QueryBuilder.Builder().insert(gato).build().toString();
+        System.out.println(query);
     }
 }
 
