d0e50adde7873699e388f959df758956dd4db33f
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2022-11-15 20:41:17 +0200

    adjustment/mysqldatabase-change to relevant ConnectionUtilities methods

--- ormsystem/src/main/java/Utils/ConnectionUtilities.java
+++ ormsystem/src/main/java/Utils/ConnectionUtilities.java
@@ -9,31 +9,6 @@ import java.sql.*;
 public class ConnectionUtilities {
     private static Logger logger = LogManager.getLogger(MysqlDatabase.class.getName());
 
-    public static <T> ResultSet TableConnectionWithSelectQuery(Connection connection, String query) throws SQLException {
-        Statement statement = connection.createStatement();
-        return statement.executeQuery(query);
-    }
-
-    public static int TableConnectionWithDeleteQuery(Connection connection, String query) throws SQLException {
-        Statement statement = connection.createStatement();
-        return statement.executeUpdate(query);
-    }
-
-    public static int TableConnectionWithCreateTableQuery(Connection connection, String query) throws SQLException {
-        Statement statement = connection.createStatement();
-        return statement.executeUpdate(query);
-    }
-
-    public static int TableConnectionWithUpdateQuery(Connection connection, String query) throws SQLException {
-        Statement statement = connection.createStatement();
-        return statement.executeUpdate(query);
-    }
-
-    public static boolean TableConnectionWithInsertQuery(Connection connection, String query) throws SQLException {
-        Statement statement = connection.createStatement();
-        return statement.execute(query);
-    }
-
     public static boolean TableConnectionWithBooleanResponse(Connection connection, String query) throws SQLException {
         Statement statement = connection.createStatement();
         return statement.execute(query);
@@ -53,10 +28,9 @@ public class ConnectionUtilities {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            logger.fatal("Connection"+ExceptionMessage.RUNTIME.getMessage());
+            logger.fatal("Connection" + ExceptionMessage.RUNTIME.getMessage());
             throw new RuntimeException(e);
         }
         return DriverManager.getConnection(SqlConfig.getUrl(), SqlConfig.getUsername(), SqlConfig.getPassword());
     }
-
 }
--- ormsystem/src/test/java/Entity/AnimalTests.java
+++ ormsystem/src/test/java/Entity/AnimalTests.java
@@ -2,22 +2,12 @@ package Entity;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AnimalTests {
-
-//    @Test
-//    void player_generateNegativeShirtNumberPlayer_ExpectIllegalArgumentException(){
-//        Integer shirtNumber = -5;
-//        int position = 1;
-//        assertThrows(IllegalArgumentException.class, () -> Player.createNamelessPlayer(position, shirtNumber), "Player's shirt number must be a positive number larger than 0");
-//    }
-
     @Test
-    void animal_setNegativeNumberOfLettersInName_ExceptIllegalArgumentException(){
+    void animal_setNegativeNumberOfLettersInName_ExceptIllegalArgumentException() {
         // given
         Animal animal = Animal.createRandomAnimalInfo();
         // then
@@ -26,7 +16,7 @@ public class AnimalTests {
 
 
     @Test
-    void animal_setNameToAnimal_ExpectTheSame(){
+    void animal_setNameToAnimal_ExpectTheSame() {
         // given
         Animal animal = Animal.createRandomAnimalInfo();
         // when
@@ -37,14 +27,18 @@ public class AnimalTests {
     }
 
     @Test
-    void animal_setNumberOfLegsToNegative_ExpectIllegalArgumentException(){
+    void animal_setNumberOfLegsToNegative_ExpectIllegalArgumentException() {
         // given
         Animal animal = Animal.createRandomAnimalInfo();
         // when
         int numOfLegs = -1;
         // then
-        assertThrows(IllegalArgumentException.class, () ->  animal.setNumOfLegs(numOfLegs));
+        assertThrows(IllegalArgumentException.class, () -> animal.setNumOfLegs(numOfLegs));
     }
 
-
+    @Test
+    void animal_setSoundNullValue_ExceptionNullPointerException() {
+        Animal animal = Animal.createRandomAnimalInfo();
+        assertThrows(NullPointerException.class, () -> animal.setSound(null));
+    }
 }
